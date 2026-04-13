61524db59f16ebafd41b9d30afecbbe9849b0b87
Justin Tung <justin.tung@live.ca>
2026-04-13 15:39:26 -0400

    feat(language): text analysis
    - Update code, reqs from upstream lab

--- src/azure-ai-102/language/01-analyze-text/text-analysis.py
+++ src/azure-ai-102/language/01-analyze-text/text-analysis.py
@@ -1,65 +1,54 @@
 from dotenv import load_dotenv
+from subprocess import call
 import os
 
 # Import namespaces
-from azure.core.credentials import AzureKeyCredential
+from azure.identity import DefaultAzureCredential
 from azure.ai.textanalytics import TextAnalyticsClient
 
 
 def main():
     try:
+        # Clear the console
+        _ = call("clear" if os.name == "posix" else "cls")
+
         # Get Configuration Settings
         load_dotenv()
-        ai_endpoint = os.getenv("AI_SERVICE_ENDPOINT")
-        ai_key = os.getenv("AI_SERVICE_KEY")
-        ai_client = None
+        foundry_endpoint = os.getenv("FOUNDRY_ENDPOINT")
 
-        if ai_key is not None and ai_endpoint is not None:
-            # Create client using endpoint and key
-            credential = AzureKeyCredential(ai_key)
-            ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
+        # Create client using endpoint
+        credential = DefaultAzureCredential()
+        ai_client = TextAnalyticsClient(
+            endpoint=foundry_endpoint, credential=credential
+        )
 
-        if ai_client is not None:
-            # Analyze each text file in the reviews folder
-            reviews_folder = "reviews"
-            for file_name in os.listdir(reviews_folder):
-                # Read the file contents
-                print("\n-------------\n" + file_name)
-                text = open(
-                    os.path.join(reviews_folder, file_name), encoding="utf8"
-                ).read()
-                print("\n" + text)
+        # Analyze each text file in the reviews folder
+        reviews_folder = "reviews"
+        for file_name in os.listdir(reviews_folder):
+            # Read the file contents
+            print("\n-------------\n" + file_name)
+            text = open(os.path.join(reviews_folder, file_name), encoding="utf8").read()
+            print("\n" + text)
 
-                # Get language
-                detectedLanguage = ai_client.detect_language(documents=[text])[0]
-                print("\nLanguage: {}".format(detectedLanguage.primary_language.name))
+            # Get language
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print("\nLanguage: {}".format(detectedLanguage.primary_language.name))
 
-                # Get sentiment
-                sentimentAnalysis = ai_client.analyze_sentiment(documents=[text])[0]
-                print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
+            # Get entities
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print("\t{} ({})".format(entity.text, entity.category))
 
-                # Get key phrases
-                phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
-                if len(phrases) > 0:
-                    print("\nKey Phrases:")
-                    for phrase in phrases:
-                        print("\t{}".format(phrase))
-
-                # Get entities
-                entities = ai_client.recognize_entities(documents=[text])[0].entities
-                if len(entities) > 0:
-                    print("\nEntities")
-                    for entity in entities:
-                        print("\t{} ({})".format(entity.text, entity.category))
-
-                # Get linked entities
-                entities = ai_client.recognize_linked_entities(documents=[text])[
-                    0
-                ].entities
-                if len(entities) > 0:
-                    print("\nLinks")
-                    for linked_entity in entities:
-                        print("\t{} ({})".format(linked_entity.name, linked_entity.url))
+            # Get PII
+            pii_result = ai_client.recognize_pii_entities(documents=[text])[0]
+            pii_entities = pii_result.entities
+            if len(pii_entities) > 0:
+                print("\nPII Entities")
+                for pii_entity in pii_entities:
+                    print("\t{} ({})".format(pii_entity.text, pii_entity.category))
+                print("Redacted Text:\n {}".format(pii_result.redacted_text))
 
     except Exception as ex:
         print(ex)
