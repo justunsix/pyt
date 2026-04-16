379b77399ac8deeb134fbc5a217231e4dd23b922
Justin Tung <justin.tung@live.ca>
2026-04-16 16:51:49 -0400

    feat(infra): add delete other RG resources

--- src/azure-ai-102/infra/providers.tf
+++ src/azure-ai-102/infra/providers.tf
@@ -1,6 +1,11 @@
 # Setup providers
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      # Allow delection of resources in group outside of Terraform
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   storage_use_azuread = true
   subscription_id     = var.subscription_id
 }
