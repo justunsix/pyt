72f9ae12850003b949045a90180bb926bd631d26
Justin Tung <justin.tung@live.ca>
2026-07-08 23:49:53 -0400

    feat(hello-world): linting

--- src/learn/helloAndAge.py
+++ src/learn/helloAndAge.py
@@ -1,17 +1,17 @@
 # This program says hello and asks for my name
 
-print('Hello World')
+print("Hello World")
 
-print('What is your name?')
+print("What is your name?")
 
 name = input()
 
-print('It is good to meet you ' + name)
-print('The length of your name is:')
+print("It is good to meet you " + name)
+print("The length of your name is:")
 
 print(len(name))
 
-print('What is your age?')
+print("What is your age?")
 age = input()
 
-print('You will be '+ str(int(age)+1)+ ' in a year')
+print("You will be " + str(int(age) + 1) + " in a year")
